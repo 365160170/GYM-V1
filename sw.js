5ba75f4481faecd9e4683cb7a7896c54d7e8a7ae
365160170 <diegodiazmarketing@gmail.com>
2026-07-03 23:52:32 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "dup-v9";
+const CACHE = "dup-v10";
 const FILES = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png", "./icon-180.png"];
 
 self.addEventListener("install", e => {
